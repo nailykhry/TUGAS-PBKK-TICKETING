6a7405f0e435cdb77491ab2fb4c33b05e4594402
rycahayash <rycahaya.205025@mhs.its.ac.id>
2023-05-31 09:27:52 +0700

    change button make payment

--- app/Views/traveller_details.php
+++ app/Views/traveller_details.php
@@ -283,7 +283,7 @@
                 </div>
 
                 <a href="#" class="mx-5 mt-3 btn btn-primary" style="background-color: #239BD8; color: white; border: none;">
-                    Continue Booking 
+                    Make Payment 
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
                     </svg>
